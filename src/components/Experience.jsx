b5c16235e7e7091b8ab763d3b89bbc00d71ac4e5
enesbasbug <enesbasbugeng@gmail.com>
2023-03-16 23:41:01 +0000

    changed experience parts visibility in mobil screen

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -162,7 +162,7 @@ const Experience = () => {
                     techs.map(({id, src, title, style}) => (
                         <div key={id} className={`shadow-md hover:scale-105 duration-500 py-2 rounded-lg ${style}`}>
                             <img src={src} alt="" className='w-20 mx-auto'/>
-                            <p className='mt-4'>{title}</p>
+                            <p className='mt-4 hidden md:block'>{title}</p>
                         </div>
                     ))
                 }
