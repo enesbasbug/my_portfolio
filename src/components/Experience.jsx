63fad061a1d1fbda8d9764b1a27f8b63be660523
enesbasbug <enesbasbugeng@gmail.com>
2023-03-18 15:19:20 +0000

    portfolio part was edited

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -149,14 +149,14 @@ const Experience = () => {
     ]
 
   return (
-    <div name="experience" className='bg-gradient-to-b from-gray-800 to-black w-full h-screen'>
+    <div name="experience" className='bg-gradient-to-b from-gray-800 to-black w-full h-screen pt-5'>
         <div className='max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full h-full text-white'>
             <div>
                 <p className='text-4xl font-bold border-b-4 border-gray-500 p-2 inline'>Experience</p>
                 <p className='py-6'>These are the technologies I've worked with</p>
             </div>
 
-            <div className='w-full grid grid-cols-5 sm:grid-cols-4 gap-8 text-center py-8 px-12 sm:px-0'>
+            <div className='w-full grid grid-cols-5 sm:grid-cols-5 gap-3 text-center py-8 px-12 sm:px-0'>
 
                 {
                     techs.map(({id, src, title, style}) => (
--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -12,26 +12,58 @@ const Portfolio = () => {
         {
             id:1,
             src:ecommerce,
+            title: "E-commerce website",
+            tech: "Python, Django, AWS Elastic Beanstalk, SQLite",
+            href: "http://djangoawsec-env.eba-ekd3hrdt.us-east-2.elasticbeanstalk.com/",
+            git_href: "https://github.com/enesbasbug/ecommerce_django",
+            c: true
         },
         {
             id:2,
-            src:estimatedBP
+            src:estimatedBP,
+            title: "A website estimates the one's blood pressure from rPPG value using Deep Learning Model by contactless webcam",
+            tech: "Python, Flask, OpenCV, Keras, Tensorflow, HTML, CSS, JavaScript ",
+            href: "https://github.com/enesbasbug/Blood_Pressure_Estimation_with_Webcam_using_Deep_Learning",
+            git_href: "https://github.com/enesbasbug/Blood_Pressure_Estimation_with_Webcam_using_Deep_Learning",
+            c: false
+
+            
         },
         {
             id:3,
-            src:recipeGenerator
+            src:recipeGenerator,
+            title: "Recipe generation and image generation based on the generated recipe using LSTM and Big Sleep",
+            tech: "Python, NLP, NLTK, NLG",
+            href: "https://enesbasbug-food-recipe-generator-app-5bw3u6.streamlit.app/",
+            git_href: "https://github.com/enesbasbug/Food_Recipe_Generator",
+            c: true
         },
         {
             id:4,
-            src:skinCancer
+            src:skinCancer,
+            title: "Skin Cancer Classifier",
+            tech: "Python, Tkinter, Keras",
+            href: "https://nbviewer.org/github/enesbasbug/Skin_Cancer_Classifier/blob/master/Classifier.ipynb",
+            git_href: "https://github.com/enesbasbug/Skin_Cancer_Classifier",
+            c: false
         },
         {
             id:5,
-            src:comedianClassifier
+            src:comedianClassifier,
+            title: "Comedian Classifier",
+            tech: "Python, OpenCV, Pandas, Numpy",
+            href: "https://github.com/enesbasbug/Image_Classification_Project",
+            git_href: "https://github.com/enesbasbug/Image_Classification_Project",
+            c: false
         },
         {
             id:6,
-            src:flutterApp
+            src:flutterApp,
+            title: "Astrology, Horoscopes and Birth Chart iOS Mobile App",
+            tech: "Flutter, Dart",
+            href: "https://github.com/enesbasbug/Flutter_iOS_App_Astrological_Sign",
+            git_href: "https://github.com/enesbasbug/Flutter_iOS_App_Astrological_Sign",
+            c: false
         },
     ]
   
@@ -44,12 +76,18 @@ const Portfolio = () => {
                 </div>
 
                 <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0'>
-                    { portfolios.map(({id, src}) => (
+                    { portfolios.map(({id, src, title, tech, href, git_href, c}) => (
                         <div key={id} className='shadow-md shadow-gray-600 rounded-lg'>
                             <img src={src} alt="" className='rounded-md duration-200 hover:scale-105'/>
+                            <p className='py-3 px-3'>{title}</p>
+                            <p className='py-3 px-3 border-r-4 font-semibold'>{tech}</p>
                             <div className='flex items-center justify-center'>
-                                <button className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105'>Demo</button>    
-                                <button className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105'>Code</button>    
+                                <button className='w-1/2 px-6 py-1 m-4 duration-200 hover:scale-105 font-bold'>
+                                    <a href={href} target="_blank" rel="noreferrer"> Demo </a>
+                                </button>    
+                                <button className='w-1/2 px-6 py-1 m-4 duration-200 hover:scale-105 font-bold'>
+                                    <a href={git_href} target="_blank" rel="noreferrer"> Code </a>
+                                </button>  
                             </div>
                         </div>
                     ))}  
